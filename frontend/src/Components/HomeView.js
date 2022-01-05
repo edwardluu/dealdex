758bfc83929b410ccbb1b4b9e18b1837c5978842
edward <thanh.luu@mail.i-on.net>
2022-01-06 00:18:19 +0700

    update custom varirans and layout

--- frontend/src/Components/HomeView.js
+++ frontend/src/Components/HomeView.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 import { Flex, Container } from "@chakra-ui/react";
-import { Button, VStack, Heading, Text, Box, Wrap, WrapItem, Table, Thead, Tbody, Tr, Th, Td, Badge } from "@chakra-ui/react";
+import { Button, VStack, Box, Wrap, WrapItem, Table, Thead, Tbody, Tr, Th, Td, Badge } from "@chakra-ui/react";
 import { ReactComponent as Logo } from "../assets/icon/DealDexLogo.svg";
 
 import DealService from "../Services/DealService";
@@ -13,7 +13,7 @@ const DummyData = [
     syndicateAddress: "MoonBoots Capital",
     isVerified: true,
     requiredNFT: "BAYC",
-    minInvestmentAmount: '10',
+    minInvestmentAmount: "10",
     paymentToken: "USDC",
     deadline: 1664879311,
   },
@@ -22,9 +22,9 @@ const DummyData = [
     syndicateAddress: "Web3 Capital",
     isVerified: false,
     requiredNFT: "BAYC",
-    minInvestmentAmount: '10k',
+    minInvestmentAmount: "10k",
     paymentToken: "USDC",
-    deadline: 1641292111,
+    deadline: 1641228522,
   },
 ];
 
@@ -44,28 +44,26 @@ const HomeView = () => {
     history.push("/createDeal");
   };
 
- 
   return (
     <Container maxW="container.xl" p={0}>
       <Flex h={{ base: "auto", md: "100%" }} py={[0, 10, 20]} direction={{ base: "column-reverse", md: "row" }}>
-        <VStack w="full" h="full" p={0} spacing={95} alignItems="flex-start">
-          <VStack w="full" h="full" alignItems="center">
+        <VStack w="full" h="full" p={0} spacing={95} alignItems="center">
+          <VStack w="800px" h="full" alignItems="center">
             <Logo />
-            <Heading as="h1" size="4xl">
+            <Box textStyle="title" pb={18}>
               DealDex
-            </Heading>
-            <Text fontSize="3xl" sx={{ padding: "18px 0 37px 0" }}>
+            </Box>
+            <Box textStyle="subtitle" pb={29}>
               Instantly turn your NFT collection into a syndicate. Our platform lets you create NFT-gated investment deals with the click of a button.
-            </Text>
-            <Button onClick={onCreateADeal} sx={{ bg: "#7879F1", color: "#fff", padding: "20px 135px" }} size="lg">
+            </Box>
+            <Button variant="dealCreate" size='lg' onClick={onCreateADeal}>
               Create a deal
             </Button>
           </VStack>
           <VStack w="full" h="full" alignItems="flex-start">
-            <Heading as="h2" size="2xl" padding="0 0 25px 0">
+            <Box textStyle="titleSection" pb={25}>
               All Deals
-            </Heading>
-
+            </Box>
             <ListDeals data={DummyData} />
           </VStack>
         </VStack>
@@ -77,68 +75,60 @@ const HomeView = () => {
 export default HomeView;
 
 const ListDeals = ({ data = [] }) => {
-  if (!data.length) return <Text fontSize="3xl">No deals created so far</Text>;
-
+  if (!data.length) return <Box textStyle="titleDeal">No deals created so far</Box>;
 
   const getTimeDeadline = (deadline = 0) => {
     const dateDeadline = new Date(deadline * 1000);
     const now = new Date();
-    const diffInSeconds = Math.abs(dateDeadline - now) / 1000;
+    const diffInSeconds = Math.floor(dateDeadline - now) / 1000;
     const days = Math.floor(diffInSeconds / 60 / 60 / 24);
+    const hours = Math.floor((diffInSeconds / 60 / 60) % 24);
+    const minutes = Math.floor((diffInSeconds / 60) % 60);
+    const seconds = Math.floor(diffInSeconds % 60);
     if (days > 0) {
-      return ( <Text>{days}d left</Text>)
+      return `${days}d left`;
+    } else if (days <= 0 && hours > 0) {
+      return `${hours}hr left`;
+    } else if (days <= 0 && hours <= 0 && minutes > 0) {
+      return `${minutes}min left`;
+    } else if (days <= 0 && hours <= 0 && minutes <= 0 && seconds > 0) {
+      return `${seconds}s left`;
     } else {
-      return ( <Text color="#E53E3E">Passed</Text>)
+      return <Box textStyle="statusDeal">Passed</Box>;
     }
-  }
-  
+  };
+
   return (
     <Wrap spacing="45px">
       {data.map((item, index) => (
         <WrapItem key={index}>
-          <Box width="610px" height="277px" borderRadius="4px" color="gray.500" bg="white" padding="26px 40px 55px" boxShadow="base" border="1px" borderColor="#E2E8F0" textAlign="left">
-            <Heading color="black" as="h4" size="xl" isTruncated>
-              {item.dealName}
-            </Heading>
+          <Box layerStyle="dealTableWrap">
+            <Box textStyle="titleDeal">{item.dealName}</Box>
             <Flex>
-              <Text fontSize="2xl" color="gray.500">
-                {item.syndicateAddress}
-              </Text>
+              <Box textStyle="subTitleDeal">{item.syndicateAddress}</Box>
               {item.isVerified && (
                 <Box ml={2}>
-                  <Badge variant="outline" background="#5D5FEF" color="white">
-                    <Text fontSize="xs">VERIFIED</Text>
-                  </Badge>
+                  <Badge variant="verified">VERIFIED</Badge>
                 </Box>
               )}
             </Flex>
 
-            <Box background="#F7FAFC" mt={25}>
-              <Table borderRadius="4px" border="1px" borderColor="#E2E8F0">
+            <Box mt={25}>
+              <Table variant="dealTable">
                 <Thead>
                   <Tr>
-                    <Th color="gray.700" fontSize="16px" borderColor="#F7FAFC" textAlign="center">
-                      Required NFT
-                    </Th>
-                    <Th color="gray.700" fontSize="16px" borderColor="#F7FAFC" textAlign="center">
-                      Min invest
-                    </Th>
-                    <Th color="gray.700" fontSize="16px" borderColor="#F7FAFC" textAlign="center">
-                      Deadline
-                    </Th>
+                    <Th>Required NFT</Th>
+                    <Th>Min invest</Th>
+                    <Th>Deadline</Th>
                   </Tr>
                 </Thead>
                 <Tbody>
                   <Tr>
-                    <Td color="gray.700" textAlign="center">
-                      {item.requiredNFT}
+                    <Td>{item.requiredNFT}</Td>
+                    <Td>
+                      {item.minInvestmentAmount} {item.paymentToken}
                     </Td>
-                    <Td color="gray.700" textAlign="center">
-                    {item.minInvestmentAmount} {item.paymentToken}
-                    </Td>
-                    <Td color="gray.700" textAlign="center">
-                      { getTimeDeadline(item.deadline)}
-                    </Td>
+                    <Td>{getTimeDeadline(item.deadline)}</Td>
                   </Tr>
                 </Tbody>
               </Table>
--- frontend/src/Utils/DealDexTheme.js
+++ frontend/src/Utils/DealDexTheme.js
@@ -1,17 +1,106 @@
-// importing the required chakra libraries
-import { theme as chakraTheme } from '@chakra-ui/react'
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
 
-// declare a variable for fonts and set our fonts. I am using Inter with various backups but you can use `Times New Roman`. Note we can set different fonts for the body and heading.
-
-// declare a variable for our theme and pass our overrides in the extendTheme method from chakra
 const DealDexTheme = extendTheme({
-  fonts: {
-    ...chakraTheme.fonts,
-    body: "Inter",
-    heading: "Inter"
-  }
-})
+  layerStyles: {
+    dealTableWrap: {
+      width: "610px",
+      height: "277px",
+      borderRadius: "4px",
+      padding: "26px 40px 55px",
+      border: "1px",
+      borderColor: "gray.200",
+      boxShadow: "base",
+      textAlign: "left",
+    },
+  },
+  textStyles: {
+    title: {
+      fontSize: "72px",
+      fontWeight: "700",
+      lineHeight: "100%",
+      color: "black",
+    },
+    titleSection: {
+      fontSize: "48px",
+      fontWeight: "700",
+      lineHeight: "100%",
+      color: "black",
+    },
+    subtitle: {
+      fontSize: "24px",
+      fontWeight: "400",
+      lineHeight: "150%",
+      color: "black",
+      letterSpacing: "1px",
+    },
+    titleDeal: {
+      fontSize: "36px",
+      fontWeight: "700",
+      lineHeight: "120%",
+      color: "black",
+    },
+    subTitleDeal: {
+      fontSize: "24px",
+      fontWeight: "400",
+      lineHeight: "150%",
+      color: "gray.500",
+    },
+    statusDeal: {
+      color: "red.500"
+    }
+  },
+  components: {
+    Button: {
+      variants: {
+        dealCreate: {
+          background: "#7879F1",
+          color: "white",
+          mt: "0px",
+          width: "380px",
+          fontWeight: "600",
+          fontSize: "18px",
+          lineHeight: "129%",
+          size:'lg'
+        },
+      },
+    },
+    Badge: {
+      variants: {
+        verified: {
+          background: "#5D5FEF",
+          color: "white",
+        },
+      },
+    },
+    Table: {
+      variants: {
+        dealTable: {
+          table: {
+            bg: "#F7FAFC",
+            borderRadius: "4px",
+            border: "1px",
+            borderColor: "gray.200",
+            boxShadow: "base",
+          },
+          th: {
+            color: "gray.700",
+            textAlign: "center",
+            fontWeight: "700",
+            fontSize: "20px",
+            lineHeight: "100%",
+            textTransform: "capitalization",
+          },
+          td: {
+            color: "gray.700",
+            textAlign: "center",
+            fontWeight: "400",
+            fontSize: "20px",
+            lineHeight: "100%",
+          },
+        },
+      },
+    },
+  },
+});
 
-// export our theme
-export default DealDexTheme
+export default DealDexTheme;
